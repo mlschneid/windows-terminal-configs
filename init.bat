f991a3143d6b1629ae859df2a213fde38b80d358
Mike Schneider <schneider707@gmail.com>
2019-06-12 17:03:49 -0700

    fix empty string comparison

--- init.bat
+++ init.bat
@@ -1,12 +1,17 @@
 @echo off
 
-set lang=%1
+REM This script to be called from the Windows Terminal profiles.json
+REM In profiles.json, set "commandline" to something like "cmd.exe /k init.bat mylangconfig"
+REM where `mylangconfig` is defined below.
+
+set lang="%1"
 set start_dir=%userprofile%\code
-
-cd %start_dir%
-
 set code=%userprofile%\code
 
+REM Set starting directory
+cd %start_dir%
+
+REM Set path env
 set PATH=%PATH%;^
 %userprofile%\bin\bin\;^
 %userprofile%\bin\vendor\git-for-windows\usr\bin\;^
@@ -18,10 +23,22 @@ REM with -x and without before it sticks
 colortool -q -x campbell.ini
 colortool -q campbell.ini
 
-if [%lang%]==csharp (
+
+REM Language specific configurations
+
+REM C# 17
+if %lang%=="csharp" (
     "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
 )
 
-if [%lang%]==csharp19 (
+REM C# 19
+if [%lang%]=="csharp19" (
     "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\Tools\VsDevCmd.bat"
 )
+
+REM Python 3.7
+if [%lang%]=="python37" (
+    set PATH="%PATH%";"%userprofile%"\AppData\Local\Programs\Python\Python37
+)
+
+REM WSL
